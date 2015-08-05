175cbd18803c6d006da7c36cabf8a6d0ec731855
newsocialsandra <sandra.e.b.svensson@gmail.com>
2015-08-05 11:41:51 +0200

    Rewrote program with list instead of variables

--- cylonprice.py
+++ cylonprice.py
@@ -1,4 +1,4 @@
-# coding: utf-8
+# # coding: utf-8
 class Product(object):
 	price = 0
 	count = 0
@@ -12,7 +12,12 @@ class Product(object):
 	def price_with_tax(self):
 		return self.price * self.count * self. vat
 
-cylon = Product(price = 900, count = 2, vat = 1.25)
-manual = Product(price = 100, count = 1, vat = 1.06)
+products = [Product(price = 900, count = 2, vat = 1.25), Product(price = 100, count = 1, vat = 1.06)]
+total_price = products[0].price_with_tax() + products[1].price_with_tax()
 
-print cylon.price_with_tax() + manual.price_with_tax()
+print total_price
+
+# cylon = Product(price = 900, count = 2, vat = 1.25)
+# manual = Product(price = 100, count = 1, vat = 1.06)
+
+# print cylon.price_with_tax() + manual.price_with_tax()
