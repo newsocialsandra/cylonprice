a8567c5426c70c2e79b4e6af697d2a3d154c0767
newsocialsandra <sandra.e.b.svensson@gmail.com>
2015-08-05 11:34:25 +0200

    Rewrote with class + constructor

--- cylonprice.py
+++ cylonprice.py
@@ -4,17 +4,15 @@ class Product(object):
 	count = 0
 	vat = 0
 
+	def __init__(self, price, count, vat):
+		self.price = price
+		self.count = count
+		self.vat = vat
+
 	def price_with_tax(self):
 		return self.price * self.count * self. vat
 
-cylon = Product()
-cylon.price = 900
-cylon.count = 2
-cylon.vat = 1.25
-
-manual = Product()
-manual.price = 100
-manual.count = 1
-manual.vat = 1.06
+cylon = Product(price = 900, count = 2, vat = 1.25)
+manual = Product(price = 100, count = 1, vat = 1.06)
 
 print cylon.price_with_tax() + manual.price_with_tax()
