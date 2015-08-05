047fd1b9a6b7e43a60296e975de7d5eb92e635a6
newsocialsandra <sandra.e.b.svensson@gmail.com>
2015-08-05 10:43:00 +0200

    Added variables

--- cylonprice.py
+++ cylonprice.py
@@ -1,1 +1,9 @@
-print 900 * 2 * 1.25 + 100 * 1.06 
+robot_price = 900
+robot_count = 2
+robot_vat = 1.25
+
+manual_price = 100
+manual_count = 1 
+manual_vat = 1.06
+
+print robot_price * robot_count * robot_vat + manual_price * manual_count * manual_vat
