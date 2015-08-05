3dd694618b26bd8d1477ee87f2a3ea3298841298
newsocialsandra <sandra.e.b.svensson@gmail.com>
2015-08-05 11:18:22 +0200

    Rewrote program using class and method

--- cylonprice.py
+++ cylonprice.py
@@ -1,9 +1,11 @@
 # coding: utf-8
-
 class Product(object):
 	price = 0
 	count = 0
 	vat = 0
+
+	def price_with_tax(self):
+		return self.price * self.count * self. vat
 
 cylon = Product()
 cylon.price = 900
@@ -15,6 +17,4 @@ manual.price = 100
 manual.count = 1
 manual.vat = 1.06
 
-print cylon.price * cylon.count * cylon.vat + manual.price * manual.count * manual.vat
-
-# ["price"] * cylon["count"] * cylon["vat"] + manual["price"] * manual["count"] * manual["vat"] 
+print cylon.price_with_tax() + manual.price_with_tax()
